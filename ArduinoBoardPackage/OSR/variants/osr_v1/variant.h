479fb0a797cfbc88c9fdc9ffb6c74371015390ab
DDeGonge888 <DDeGonge888@gmail.com>
2020-10-24 10:53:43 -0400

    update to 1.0.3

--- ArduinoBoardPackage/OSR/variants/osr_v1/variant.h
+++ ArduinoBoardPackage/OSR/variants/osr_v1/variant.h
@@ -16,8 +16,8 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#ifndef _VARIANT_OSR_V1_
-#define _VARIANT_OSR_V1_
+#ifndef _VARIANT_FEATHER_M4_
+#define _VARIANT_FEATHER_M4_
 
 // The definitions here needs a SAMD core >=1.6.10
 #define ARDUINO_SAMD_VARIANT_COMPLIANCE 10610
@@ -57,66 +57,64 @@ extern "C"
  *----------------------------------------------------------------------------*/
 
 // Number of pins defined in PinDescription array
-#define PINS_COUNT           (45u)
-#define NUM_DIGITAL_PINS     (20u)
-#define NUM_ANALOG_INPUTS    (0u)
+#define PINS_COUNT           (49u)
+#define NUM_DIGITAL_PINS     (29u)
+#define NUM_ANALOG_INPUTS    (1u)
 #define NUM_ANALOG_OUTPUTS   (1u)
-#define analogInputToDigitalPin(p)  ((p < 6u) ? (p) + 14u : -1) // This is old bad nogood
+#define analogInputToDigitalPin(p)  ((p < 6u) ? (p) + 14u : -1)
 
-#define FIRST_DIGITAL_PIN 26
-
-#define digitalPinToPort(P)        ( &(PORT->Group[g_APinDescription[P + FIRST_DIGITAL_PIN].ulPort]) )
-#define digitalPinToBitMask(P)     ( 1 << g_APinDescription[P + FIRST_DIGITAL_PIN].ulPin )
+#define digitalPinToPort(P)        ( &(PORT->Group[g_APinDescription[P].ulPort]) )
+#define digitalPinToBitMask(P)     ( 1 << g_APinDescription[P].ulPin )
 //#define analogInPinToBit(P)        ( )
 #define portOutputRegister(port)   ( &(port->OUT.reg) )
 #define portInputRegister(port)    ( &(port->IN.reg) )
 #define portModeRegister(port)     ( &(port->DIR.reg) )
-#define digitalPinHasPWM(P)        ( g_APinDescription[P + FIRST_DIGITAL_PIN].ulPWMChannel != NOT_ON_PWM || g_APinDescription[P + FIRST_DIGITAL_PIN].ulTCChannel != NOT_ON_TIMER )
+#define digitalPinHasPWM(P)        ( g_APinDescription[P].ulPWMChannel != NOT_ON_PWM || g_APinDescription[P].ulTCChannel != NOT_ON_TIMER )
 
-// // LEDs
-#define PIN_LED_13           (39u)
-// #define PIN_LED_RXL          (25u)
-// #define PIN_LED_TXL          (26u)
-#define PIN_LED              PIN_LED_13
-// #define PIN_LED2             PIN_LED_RXL
-// #define PIN_LED3             PIN_LED_TXL
-#define LED_BUILTIN          PIN_LED_13
-// #define PIN_NEOPIXEL         (8)
+/*
+ * digitalPinToTimer(..) is AVR-specific and is not defined for SAMD
+ * architecture. If you need to check if a pin supports PWM you must
+ * use digitalPinHasPWM(..).
+ *
+ * https://github.com/arduino/Arduino/issues/1833
+ */
+// #define digitalPinToTimer(P)
+
+// LEDs
+#define PIN_LED_14           (14u)
+#define PIN_LED              PIN_LED_14
+#define LED_BUILTIN          PIN_LED_14
+
 
 /*
  * Analog pins
  */
-#define PIN_A0               (46ul)
-// #define PIN_A1               (PIN_A0 + 1)
-// #define PIN_A2               (PIN_A0 + 2)
-// #define PIN_A3               (PIN_A0 + 3)
-// #define PIN_A4               (PIN_A0 + 4)
-// #define PIN_A5               (PIN_A0 + 5)
-// #define PIN_A6               (PIN_A0 + 6)
+#define PIN_A0               (17ul)
+#define PIN_A1               (PIN_A0 + 1)
 
-#define PIN_DAC0             (11ul)
-#define PIN_DAC1             (47ul)
+#define PIN_DAC0             PIN_A0
+#define PIN_DAC1             PIN_A1
 
 static const uint8_t A0  = PIN_A0;
-// static const uint8_t A1  = PIN_A1;
-// static const uint8_t A2  = PIN_A2;
-// static const uint8_t A3  = PIN_A3;
-// static const uint8_t A4  = PIN_A4;
-// static const uint8_t A5  = PIN_A5;
-// static const uint8_t A6  = PIN_A6 ;
+static const uint8_t A1  = PIN_A1;
 
 static const uint8_t DAC0 = PIN_DAC0;
 static const uint8_t DAC1 = PIN_DAC1;
 
 #define ADC_RESOLUTION		12
 
+
+// Other pins
+#define PIN_ATN              (27ul)
+static const uint8_t ATN = PIN_ATN;
+
 // Stepper driver pins
-#define D0_EN                (14u)
+#define D0_EN                (29u)
 #define D0_S0_STEP           (D0_EN + 1)
 #define D0_S0_DIR            (D0_EN + 2)
 #define D0_S1_STEP           (D0_EN + 3)
 #define D0_S1_DIR            (D0_EN + 4)
-#define D1_EN                (20u)
+#define D1_EN                (35u)
 #define D1_S0_STEP           (D1_EN + 1)
 #define D1_S0_DIR            (D1_EN + 2)
 #define D1_S1_STEP           (D1_EN + 3)
@@ -134,11 +132,11 @@ static const uint8_t D1S1S = D1_S1_STEP ;
 static const uint8_t D1S1D = D1_S1_DIR ;
 
 // Peripheral pins
-#define Servo_0              (40u)
+#define Servo_0              (15u)
 #define Servo_1              (Servo_0 + 1)
 #define Servo_2              (Servo_0 + 2)
-#define Gate_0               (Servo_0 + 3)
-#define Gate_1               (Servo_0 + 4)
+#define Gate_0               (18u)
+#define Gate_1               (Gate_0 + 1)
 
 static const uint8_t S0 = Servo_0 ;
 static const uint8_t S1 = Servo_1 ;
@@ -146,17 +144,13 @@ static const uint8_t S2 = Servo_2 ;
 static const uint8_t G0 = Gate_0 ;
 static const uint8_t G1 = Gate_1 ;
 
-// AREF
-#define PIN_ATN              (10ul)
-static const uint8_t ATN = PIN_ATN;
-
 /*
  * Serial interfaces
  */
 
 // Serial1
-#define PIN_SERIAL1_RX       (0ul)
-#define PIN_SERIAL1_TX       (1ul)
+#define PIN_SERIAL1_RX       (15ul)
+#define PIN_SERIAL1_TX       (16ul)
 #define PAD_SERIAL1_RX       (SERCOM_RX_PAD_1)
 #define PAD_SERIAL1_TX       (UART_TX_PAD_0)
 
@@ -165,16 +159,16 @@ static const uint8_t ATN = PIN_ATN;
  */
 #define SPI_INTERFACES_COUNT 1
 
-#define PIN_SPI_MISO         (4u)
-#define PIN_SPI_MOSI         (5u)
-#define PIN_SPI_SCK          (6u)
+#define PIN_SPI_MISO         (21u)
+#define PIN_SPI_MOSI         (22u)
+#define PIN_SPI_SCK          (23u)
 #define PERIPH_SPI           sercom1
 #define PAD_SPI_TX           SPI_PAD_3_SCK_1
 #define PAD_SPI_RX           SERCOM_RX_PAD_2
 
-#define SD_CHIP_SEL          (25u)
-#define D0_CHIP_SEL          (13u)
-#define D1_CHIP_SEL          (19u)
+#define SD_CHIP_SEL          (40u)
+#define D0_CHIP_SEL          (28u)
+#define D1_CHIP_SEL          (34u)
 
 static const uint8_t MOSI = PIN_SPI_MOSI ;
 static const uint8_t MISO = PIN_SPI_MISO ;
@@ -189,8 +183,8 @@ static const uint8_t D1CS	= D1_CHIP_SEL;
  */
 #define WIRE_INTERFACES_COUNT 1
 
-#define PIN_WIRE_SDA         (2u)
-#define PIN_WIRE_SCL         (3u)
+#define PIN_WIRE_SDA         (19u)
+#define PIN_WIRE_SCL         (20u)
 #define PERIPH_WIRE          sercom2
 #define WIRE_IT_HANDLER      SERCOM2_Handler
 #define WIRE_IT_HANDLER_0    SERCOM2_0_Handler
@@ -204,40 +198,17 @@ static const uint8_t SCL = PIN_WIRE_SCL;
 /*
  * USB
  */
-#define PIN_USB_HOST_ENABLE (7ul)
-#define PIN_USB_DM          (8ul)
-#define PIN_USB_DP          (9ul)
+#define PIN_USB_HOST_ENABLE (24ul)
+#define PIN_USB_DM          (25ul)
+#define PIN_USB_DP          (26ul)
 
 /*
  * I2S Interfaces
  */
-// #define I2S_INTERFACES_COUNT 1
+#define I2S_INTERFACES_COUNT 0
 
-// #define I2S_DEVICE          0
-// #define I2S_CLOCK_GENERATOR 3
-
-// #define PIN_I2S_SDO          (11u)
-// #define PIN_I2S_SDI          (12u)
-// #define PIN_I2S_SCK          PIN_SERIAL1_TX
-// #define PIN_I2S_FS           (10u)
-// #define PIN_I2S_MCK          PIN_SERIAL1_RX
-
-// On-board QSPI Flash
-// #define EXTERNAL_FLASH_DEVICES   GD25Q16C
-// #define EXTERNAL_FLASH_USE_QSPI
-
-//QSPI Pins
-// #define PIN_QSPI_SCK    (34u)
-// #define PIN_QSPI_CS     (35u)
-// #define PIN_QSPI_IO0    (36u)
-// #define PIN_QSPI_IO1    (37u)
-// #define PIN_QSPI_IO2    (38u)
-// #define PIN_QSPI_IO3    (39u)
-
-#if !defined(VARIANT_QSPI_BAUD_DEFAULT)
-  // TODO: meaningful value for this
-  #define VARIANT_QSPI_BAUD_DEFAULT 5000000
-#endif
+#define I2S_DEVICE          0
+#define I2S_CLOCK_GENERATOR 3
 
 #ifdef __cplusplus
 }
